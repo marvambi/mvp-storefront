e3fb95c8e2221de2bdcaae3c16399143b327dd54
Marvin Ambrose <marvambi@gmail.com>
2023-02-23 18:20:33 +0100

    update: updated login component with conditional rendering for actions login/register

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,28 +1,46 @@
 // app/routes/login.tsx
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { FormField } from '~/components/form-field';
 import { Layout } from '~/components/layout'
-import { FormField } from '~/components/form-field'
-
 export default function Login() {
   const [action, setAction] = useState('login');
+  const [role, setRole] = useState<String>('');
   const [formData, setFormData] = useState({
     email: '',
     password: '',
     username: '',
-    role: '',
+    role,
   })
 
   // Updates the form data when an input changes
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, field: string) => {
-    console.log("Form vals: ", formData);
     setFormData(form => ({ ...form, [field]: event.target.value }))
 
   }
-  const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("Form vals: ", formData);
-    setFormData(form => ({ ...form, role: event.target.value }));
-  }
 
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setRole(event.target.value);
+    //setFormData(form => ({ ...form, role: event.target.value }));
+
+  }
+  useEffect(() => {
+    setFormData(form => ({ ...form, role: role }));
+  }, [role]);
+  useEffect(() => {
+    return console.log("New state: ", formData);
+  }, [formData]);
+
+  const actions = () => {
+    let decision = false;
+    if (action === 'login') {
+      decision = (formData.email == "" || formData.password == "") ? true : false;
+    }
+    if (action === 'register') {
+      decision = (formData.email == "" || formData.password == "" || formData.username == "" || formData.role == "") ? true : false;
+    }
+    return decision;
+  };
+  const determine = actions();
 
   return (
     <Layout>
@@ -33,60 +51,56 @@ export default function Login() {
         >
           {action === 'login' ? 'Sign Up' : 'Sign In'}
         </button>
-        <h2 className="text-5xl font-extrabold text-yellow-300">Welcome to Vender!</h2>
+        <h2 className="text-5xl font-extrabold text-yellow-300">Vender...</h2>
         <p className="font-semibold text-slate-300">
           {action === 'login' ? 'Log In To Buy/Sell Some Gizmos!' : 'Sign Up To Get Started!'}
         </p>
 
         <form method="post" className="rounded-2xl bg-gray-200 p-6 w-96">
-          <label htmlFor={'email'} className="text-blue-600 font-semibold">
-            Email
-          </label><br></br>
-          <input
-            name="email"
+
+          <FormField
+            label='Email'
+            type='email'
+            htmlFor="email"
             value={formData.email}
-            className="w-full p-2 rounded-xl my-2"
             onChange={e => handleInputChange(e, 'email')}
-          /><br></br>
-          <label htmlFor={'password'} className="text-blue-600 font-semibold">
-            Password
-          </label><br></br>
-          <input
+          />
+
+          <FormField
+            label='Password'
             type="password"
-            name="password"
+            htmlFor="password"
             value={formData.password}
-            className="w-full p-2 rounded-xl my-2"
             onChange={e => handleInputChange(e, 'password')}
           />
           {action === 'register' && (
             <>
-              <label htmlFor={'username'} className="text-blue-600 font-semibold">
-                Username
-              </label><br></br>
-              <input
-                name="username"
-                type="text"
-                className="w-full p-2 rounded-xl my-2"
-                onChange={e => handleInputChange(e, 'username')}
+              <FormField
+                htmlFor="username"
+                type='text'
+                label="Username"
+                onChange={(e) => handleInputChange(e, 'username')}
                 value={formData.username}
-              /><br></br>
-              <p className="text-blue-600 font-semibold mt-5">
-                <label htmlFor={'role'} className="text-blue-600 font-semibold">
-                  Select a Role 👉
-                </label>&nbsp;&nbsp;
-                <label>
-                  <input onChange={(e) => handleSelectChange(e)} type="radio" name="role" defaultChecked={true} value={formData.role} />
-                  Seller
-                </label>&nbsp;&nbsp;
-                <label>
-                  <input onChange={(e) => handleSelectChange(e)} type="radio" name="role" value={formData.role} />
-                  Buyer
-                </label>
-              </p>
+              />
+
+              <select className="w-half p-2 rounded-xl my-2 items-center"
+                onChange={(e) => {
+                  e.target.value !== "" && handleSelectChange(e)
+                }}
+              >
+                <option className="text-blue-600 font-semibold mt-5" value="">Select a role</option>
+                <option className="text-blue-600 font-semibold mt-5" key={'seller'} value={'seller'}>
+                  {'Seller'}
+                </option>
+                &nbsp;&nbsp;
+                <option className="text-blue-600 font-semibold mt-5" key={'buyer'} value={'buyer'}>
+                  {'Buyer'}
+                </option>
+              </select>
             </>
           )}
           <div className="w-full text-center">
-            <button type="submit" name="_action" value={action} className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1 mt-5">
+            <button disabled={determine} type="submit" name="_action" value={action} className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1 mt-5">
               {
                 action === 'login' ? "Sign In" : "Sign Up"
               }
