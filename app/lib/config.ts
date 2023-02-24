0f365d446205c2826ff7a7d7d22fb52874b1e97e
Marvin Ambrose <marvambi@gmail.com>
2023-02-24 17:05:58 +0100

    good

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:3001/';
+const API_URL = 'http://127.0.0.1:3000';
 
 
 export { API_URL, };
--- app/utils/types.server.ts
+++ app/utils/types.server.ts
@@ -10,3 +10,15 @@ export type LoginForm = {
   email: string
   password: string
 }
+
+export type Product = {
+  _id: string
+  sellerId: string
+  productName: string
+  amountAvailable: string
+  cost: string
+  description: string
+  createdAt: string
+  updatedAt: string
+  __v?: number
+}
